ea1a8132be26654e07a1f508bffef3eac820c20f
Xaver Gruber <xaver.max.gruber@gmail.com>
2024-03-31 22:38:51 +0200

    remove python test_* file

--- test_test.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    def test_always_fails(self):
-        self.assertTrue(False)
