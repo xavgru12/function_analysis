01ac96d2a10eb194f818a4e0c62d2de5ccfa4426
Xaver Gruber <xaver.max.gruber@gmail.com>
2024-03-31 20:05:00 +0200

    test

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
 import requests
-print("hello world!")
-myVariable = 2
 
-
-# Input
-def _make_ssl_transport(
-    rawsock, protocol, sslcontext, waiter=None,
-    *, server_side=False, server_hostname=None,
-    extra=None, server=None,
-    ssl_handshake_timeout=None,
-    call_connection_made=True):
-        pass
