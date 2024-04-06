eac65876e1f2bb467f9e1d6a333067297e479604
Xaver Gruber <xaver.max.gruber@gmail.com>
2024-04-06 18:08:36 +0200

    add pandas to poetry

--- sqlDatabase.py
+++ sqlDatabase.py
@@ -3,9 +3,13 @@ import os
 import commandLineArgumentParser
 from typing import Optional
 from typing import Any
+import pandas
 
 class SqlDatabase:
     def __init__(self):
+        self.train_dataframe = Optional[Any]
+        self.ideal_dataframe = Optional[Any]
+        self.test_dataframe = Optional[Any]
         self.command_line_argument_parser = commandLineArgumentParser.CommandLineArgumentParser()
         arguments = self.command_line_argument_parser.readArguments() 
         if arguments.csv:
@@ -21,18 +25,25 @@ class SqlDatabase:
         return None 
 
     def write_csv(self, path):
-        train = os.path.join(path, "train.csv")
-        if not os.path.isfile(train):
-            raise FileNotFoundError(train) 
+        train_csv = os.path.join(path, "train.csv")
+        if not os.path.isfile(train_csv):
+            raise FileNotFoundError(train_csv) 
 
-        ideal = os.path.join(path, "ideal.csv")
-        if not os.path.isfile(ideal):
-            raise FileNotFoundError(ideal) 
+        ideal_csv = os.path.join(path, "ideal.csv")
+        if not os.path.isfile(ideal_csv):
+            raise FileNotFoundError(ideal_csv) 
 
-        test = os.path.join(path, "test.csv")
-        if not  os.path.isfile(test):
-            raise FileNotFoundError(test)
+        test_csv = os.path.join(path, "test.csv")
+        if not os.path.isfile(test_csv):
+            raise FileNotFoundError(test_csv)
 
+
+        with open(train_csv, 'r') as file:
+            self.train_dataframe = pandas.read_csv(file, sep=',', encoding="UTF-8")
+            print(self.train_dataframe)
+
+
+         
 
     def write(self):
         # write to sql
