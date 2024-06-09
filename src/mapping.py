84cc78324bd6d1a0e177e57fe7765abf89fac0de
Xaver Gruber <xaver.max.gruber@gmail.com>
2024-06-09 20:05:42 +0200

    lint files

--- src/mapping.py
+++ src/mapping.py
@@ -1,17 +1,25 @@
+import math
+
 from .database import Database
 from .model.test_map import TestMap
-import math
 
 
 class Mapping:
+    """
+    Uses the database to do calculations.
+
+    smallest mean squared error, max delta and match test data.
+    """
+
     def __init__(self, training_index, database):
+        """Init the mapping class."""
         self.training_index = training_index
         self.database = database
         self.ideal_index = None
         self.max_delta = None
         self.test_map_list = []
 
-    def calculate_mean_squared_error(self, ideal_list_index):
+    def __calculate_mean_squared_error(self, ideal_list_index):
         error = 0
         x_y_pairs_length = len(self.database.train_list[self.training_index].x_y_pairs)
         for key, value in self.database.train_list[
@@ -21,16 +29,17 @@ class Mapping:
                 value - self.database.ideal_list[ideal_list_index].x_y_pairs[key]
             ) ** 2
 
-        mean_squared_error = error / x_y_pairs_length
-        return mean_squared_error
+        return error / x_y_pairs_length
+
 
     def find_smallest_mean_squared_error(self):
+        """Find the smallest mean squared error."""
         smallest_mean_squared_error = None
 
         for ideal_index in range(len(self.database.ideal_list)):
-            mean_squared_error = self.calculate_mean_squared_error(ideal_index)
+            mean_squared_error = self.__calculate_mean_squared_error(ideal_index)
 
-            if smallest_mean_squared_error == None:
+            if smallest_mean_squared_error is None:
                 self.smallest_mean_squared_error = smallest_mean_squared_error = (
                     mean_squared_error
                 )
@@ -41,15 +50,19 @@ class Mapping:
                 self.ideal_index = ideal_index
                 self.smallest_mean_squared_error = smallest_mean_squared_error
 
-        if smallest_mean_squared_error == None:
+        if smallest_mean_squared_error is None:
+            error_message = "error: smallest_mean_squared_error could not be calculated"
             raise ValueError(
-                "error: smallest_mean_squared_error could not be calculated"
+                error_message,
             )
 
     def find_matching_test_coordinates(self):
+        """Find matching test coordinates."""
         if self.max_delta is None or self.ideal_index is None:
+            error_message = """error: max_delta or ideal index is not set,
+                               cannot find matching test coordinates"""
             raise ValueError(
-                "error: max_delta or ideal index is not set, cannot find matching test coordinates"
+                error_message,
             )
 
         ideal_x_y_pairs = self.database.ideal_list[self.ideal_index].x_y_pairs
@@ -64,9 +77,12 @@ class Mapping:
                 self.test_map_list.append(test_map)
 
     def find_max_delta(self):
-        if self.ideal_index == None:
+        """Find max delta."""
+        if self.ideal_index is None:
+            error_message = """error: self.index is not set,
+                               cannot calculate find_max_delta"""
             raise ValueError(
-                "error: self.index is not set, cannot calculate find_max_delta"
+                error_message,
             )
 
         max_delta = None
@@ -75,17 +91,18 @@ class Mapping:
             self.training_index
         ].x_y_pairs.items():
             delta = abs(
-                value - self.database.ideal_list[self.ideal_index].x_y_pairs[key]
+                value - self.database.ideal_list[self.ideal_index].x_y_pairs[key],
             )
 
-            if max_delta == None:
+            if max_delta is None:
                 max_delta = delta
 
             if delta > max_delta:
                 max_delta = delta
 
-        if max_delta == None:
-            raise ValueError("error: max delta could not be calculated")
+        if max_delta is None:
+            error_message = "error: max delta could not be calculated"
+            raise ValueError(error_message)
         self.max_delta = max_delta
 
 
